ae989b2f3e165ee8d87be87e6548909eaa491aba
João Cardoso <joaog.zamp@gmail.com>
2026-01-12 16:35:37 -0300

    ajustes merge

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
-=======
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
@@ -39,6 +14,5 @@ const eslintConfig = defineConfig([
     "next-env.d.ts",
   ]),
 ]);
->>>>>>> updating-next
 
 export default eslintConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,7 @@
 const config = {
-<<<<<<< HEAD
-  plugins: ["@tailwindcss/postcss"],
-=======
   plugins: {
     "@tailwindcss/postcss": {},
   },
->>>>>>> updating-next
 };
 
 export default config;
